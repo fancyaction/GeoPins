fee6eab1b605c4bf4ef3f421bfc773ffabf10077
fancyaction <labcoatdisaster@gmail.com>
2020-02-21 17:04:41 -0600

    Connect to mongoDB server

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const { ApolloServer } = require('apollo-server')
-
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('DB connected!'))
+.catch(err => console.error(err));
 
 const server = new ApolloServer({
     typeDefs,
