abd680793707dda8aa8b5311abfeb10995f404f4
fancyaction <labcoatdisaster@gmail.com>
2020-02-23 10:14:51 -0600

    Setup google login

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,19 +1,22 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
-// import Typography from "@material-ui/core/Typography";
+import React from 'react';
+import { GoogleLogin } from 'react-google-login';
+import { withStyles } from '@material-ui/core/styles';
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+    const onSuccess = googleUser => {
+        const token = googleUser.getAuthResponse().id_token;
+    };
+    return <GoogleLogin clientId={process.env.REACT_APP_OAUTH_CLIENT_ID} onSuccess={onSuccess} isSignedIn={true} />;
 };
 
 const styles = {
-  root: {
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "column",
-    alignItems: "center"
-  }
+    root: {
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center'
+    }
 };
 
 export default withStyles(styles)(Login);
