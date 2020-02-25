832b529184c5820a432b27c458ebcfb74d788195
fancyaction <labcoatdisaster@gmail.com>
2020-02-25 14:53:49 -0600

    Update no content view with text

--- client/src/components/Pin/NoContent.js
+++ client/src/components/Pin/NoContent.js
@@ -1,21 +1,28 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
-// import Explore from "@material-ui/icons/Explore";
-// import Typography from "@material-ui/core/Typography";
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import ExploreIcon from '@material-ui/icons/Explore';
+import Typography from '@material-ui/core/Typography';
 
-const NoContent = ({ classes }) => <div>NoContent</div>;
+const NoContent = ({ classes }) => (
+    <div className={classes.root}>
+        <ExploreIcon className={classes.icon} />
+        <Typography component="h2" variant="h6" align="center" color="textPrimary" gutterBottom noWrap>
+            Click on map to add a pin
+        </Typography>
+    </div>
+);
 
 const styles = theme => ({
-  root: {
-    display: "flex",
-    alignItems: "center",
-    flexDirection: "column",
-    justifyContent: "center"
-  },
-  icon: {
-    margin: theme.spacing.unit,
-    fontSize: "80px"
-  }
+    root: {
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'center'
+    },
+    icon: {
+        margin: theme.spacing.unit,
+        fontSize: '80px'
+    }
 });
 
 export default withStyles(styles)(NoContent);
