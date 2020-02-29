6f84dd1965d2df6dca43af56ee70750205216f00
fancyaction <labcoatdisaster@gmail.com>
2020-02-29 11:32:07 -0600

    Connect apollo; setup websocket link

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,10 @@
 import React, { useContext, useReducer } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { WebSocketLink } from 'apollo-link-ws';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import * as serviceWorker from './serviceWorker';
 import UserContext from './context';
@@ -9,19 +13,33 @@ import App from './pages/App';
 import Splash from './pages/Splash';
 import ProtectedRoute from './ProtectedRoute';
 
+const wsLink = new WebSocketLink({
+    uri: 'ws://localhost:4000/graphql',
+    options: {
+        reconnect: true
+    }
+});
+
+const client = new ApolloClient({
+    link: wsLink,
+    cache: new InMemoryCache()
+});
+
 const Root = () => {
     const initialState = useContext(UserContext);
     const [state, dispatch] = useReducer(userReducer, initialState);
 
     return (
-        <UserContext.Provider value={{ state, dispatch }}>
-            <Router>
-                <Switch>
-                    <ProtectedRoute exact path="/" component={App} />
-                    <Route path="/login" component={Splash} />
-                </Switch>
-            </Router>
-        </UserContext.Provider>
+        <Router>
+            <ApolloProvider client={client}>
+                <UserContext.Provider value={{ state, dispatch }}>
+                    <Switch>
+                        <ProtectedRoute exact path="/" component={App} />
+                        <Route path="/login" component={Splash} />
+                    </Switch>
+                </UserContext.Provider>
+            </ApolloProvider>
+        </Router>
     );
 };
 
